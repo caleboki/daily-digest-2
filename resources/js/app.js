100d5d9a27a1df47cd758fffbd545fde64735e47
Caleb Oki <caleb.oki@ibm.com>
2023-11-05 15:53:22 -0500

    installed vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,13 @@
 import './bootstrap';
-import '../css/app.css'; 
+import '../css/app.css';
+
+import { createApp } from 'vue';
+import Index from './components/Index.vue';
+
+const app = createApp({
+    components: {
+        Index,
+    }
+});
+
+app.mount("#app");
---
+++ resources/js/components/Index.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>Index Component</h1>
+</template>
